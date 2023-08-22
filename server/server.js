835d94b23e129f4fb758228af03c6b61ee520389
aashishrana <aashishrana71135@gmail.com>
2023-08-21 17:05:29 -0700

    database connected to the local database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 // const app = require("./app.js");
 import app from "./app.js"
+import mongoose from "mongoose";
+import connectionToDB from "./config/dbConnection.js";
 
 const PORT = process.env.PORT|| 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+    await connectionToDB();
     console.log(`APP is running at http:localhost : ${PORT}`);
 });
