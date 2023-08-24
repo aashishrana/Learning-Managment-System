a031abc4d5e68b908abab63eaefd37902d431052
aashishrana <aashishrana71135@gmail.com>
2023-08-24 15:26:45 -0700

    working in between

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ config();
 import cookieParser  from "cookie-parser";
 import morgan from "morgan";
 import router from "./routes/user.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 
 const app = express();
@@ -36,6 +37,8 @@ app.use("/api/v1/user", router)
 app.all("*" , (req , res) => {
     res.status(404).send("OOPS !! 404 page not found");
 });
+
+app.use(errorMiddleware);
 
 // module.exports = app
 export default app;
