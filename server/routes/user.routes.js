fc35b633e6d82c6552414e03574d43dba765eb6f
Aashish rana <aashishrana71135@gmail.com>
2023-11-20 07:37:01 +0000

    forgot routes and reset routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,13 +2,16 @@ import express from  "express";
 import { Router } from "express";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
-import {register , login , logout , getProfile} from "../controllers/user.controller.js"
+import {register , login , logout , getProfile, forgotPassword, resetPassword} from "../controllers/user.controller.js"
+import upload from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register",upload.single("avatar"), register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/me",isLoggedIn, getProfile)
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password" , resetPassword);
 
 export default router;
