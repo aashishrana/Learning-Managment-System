631e6628fbd127de43ce8cd5d5c7f831af60eb06
Aashish rana <aashishrana71135@gmail.com>
2023-11-21 12:07:16 +0530

    working with change password route

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,8 @@
 import {Schema, model} from "mongoose";
 import bcrypt from "bcryptjs"
 import  jwt  from "jsonwebtoken";
+import crypto from "crypto";
+
 const userSchema = new Schema( {
     fullName : {
         type : "String",
